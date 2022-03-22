1aa9d9989cd5d1bc3b3ba346809b944405c3bc54
CarolynnFleming <cfcatpink@gmail.com>
2022-03-22 12:38:03 -0700

    single id

--- __tests__/publisher.test.js
+++ __tests__/publisher.test.js
@@ -57,4 +57,23 @@ describe('local-bookstore routes', () => {
 
 
   });
+
+  it('should fetch a single publisher by id', async () => {
+    const publisher = await Publisher.insert({ name:  'Danial J. Barrett',
+    city: 'New York',
+    state: 'NY',
+    country: 'SW', });
+
+    const expected = {
+      publisher_id: expect.any(String),
+      name: 'Danial J. Barrett',
+      city: 'New York',
+      state: 'NY',
+      country: 'SW',
+    };
+
+    const res = await request(app).get(`/api/v1/publishers/${publisher.publisher_id}`);
+
+  expect(res.body).toEqual(expected);
+  });
 });
