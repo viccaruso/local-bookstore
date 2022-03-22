e204a5b80266b0d5a6ddc3b7fd0efc11103cdb2b
CarolynnFleming <cfcatpink@gmail.com>
2022-03-22 12:17:54 -0700

    get

--- __tests__/publisher.test.js
+++ __tests__/publisher.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Publisher = require('../lib/models/Publisher');
 
 describe('local-bookstore routes', () => {
   beforeEach(() => {
@@ -28,5 +29,36 @@ describe('local-bookstore routes', () => {
       country: 'SW',
     };
     expect(res.body).toEqual(expected);
+  });
+
+  it('should fetch a lisst of all publishers', async () => {
+    await Publisher.insert({ name: 'Danial J. Barrett',
+      city: 'New York',
+      state: 'NY',
+      country: 'SW', });
+    await Publisher.insert({ name: 'Michael Brady',
+      city: 'Detroit',
+      state: 'MI',
+      country: 'US', });
+
+    const expected = [{
+      publisher_id: expect.any(String),
+      name: 'Danial J. Barrett',
+      city: 'New York',
+      state: 'NY',
+      country: 'SW',
+    }, {
+      publisher_id: expect.any(String),
+      name: 'Michael Brady',
+      city: 'Detroit',
+      state: 'MI',
+      country: 'US', }];
+
+    const res = await request(app).get(
+      '/api/v1/publishers'
+    );
+    expect(res.body).toEqual(expected);
+
+
   });
 });
