e848b77230bbcb40901da852af5ef81b1c2cddbd
Victor Caruso <vic.caruso@gmail.com>
2022-03-24 16:58:58 -0700

    set up files for controller, model, and test

---
+++ __tests__/review.test.js
@@ -0,0 +1,16 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Review = require('../lib/models/Review');
+
+describe('local-bookstore routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+});
