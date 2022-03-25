5236e2d9f8fb485a9703830acb66fc31bca32dd6
James Keicher <jamesdkeicher@gmail.com>
2022-03-25 09:01:00 -0700

    test: Should update reviewer by id

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -4,7 +4,6 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Reviewer = require('../lib/models/Reviewer');
 
-
 describe('local-bookstore routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -15,23 +14,28 @@ describe('local-bookstore routes', () => {
   });
 
   it('should be able insert a new reviwer into reviewer', async () => {
-    const res = await request(app).post('/api/v1/reviewers').send({ name: 'Bob Dylan', company: 'STAY ALIVE INC.' });
+    const res = await request(app)
+      .post('/api/v1/reviewers')
+      .send({ name: 'Bob Dylan', company: 'STAY ALIVE INC.' });
 
     const expected = {
       reviewer_id: expect.any(String),
       name: 'Bob Dylan',
-      company: 'STAY ALIVE INC.'
+      company: 'STAY ALIVE INC.',
     };
     expect(res.body).toEqual(expected);
-
   });
 
   it('fetch a list of all reviewers', async () => {
     await Reviewer.insert({
-      name: 'Bob Dylan', company: 'STAY ALIVE INC.'
+      name: 'Bob Dylan',
+      company: 'STAY ALIVE INC.',
     });
 
-    const expected = [{ 'company': 'LRF', 'name': 'Jack', 'reviewer_id': '1' }, { 'company': 'STAY ALIVE INC.', 'name': 'Bob Dylan', 'reviewer_id': '2' }];
+    const expected = [
+      { company: 'LRF', name: 'Jack', reviewer_id: '1' },
+      { company: 'STAY ALIVE INC.', name: 'Bob Dylan', reviewer_id: '2' },
+    ];
 
     const res = await request(app).get('/api/v1/reviewers');
 
@@ -39,16 +43,44 @@ describe('local-bookstore routes', () => {
   });
 
   it('Gets a reviewer by id that includes all their reviews', async () => {
-    const reviewer = await Reviewer.insert({ name: 'Bob Dylan', company: 'STAY ALIVE INC.' });
+    const reviewer = await Reviewer.insert({
+      name: 'Bob Dylan',
+      company: 'STAY ALIVE INC.',
+    });
 
     const expected = {
-      'reviewer_id': expect.any(String),
-      'name': 'Bob Dylan',
-      'company': 'STAY ALIVE INC.',
-      'review': expect.any(Array)
+      reviewer_id: expect.any(String),
+      name: 'Bob Dylan',
+      company: 'STAY ALIVE INC.',
+      review: expect.any(Array),
     };
 
-    const res = await request(app).get(`/api/v1/reviewers/${reviewer.reviewer_id}`);
+    const res = await request(app).get(
+      `/api/v1/reviewers/${reviewer.reviewer_id}`
+    );
+
+    expect(res.body).toEqual(expected);
+  });
+
+  it('Should update a reviewers data', async () => {
+    const reviewer = Reviewer.insert({
+      name: 'Jaques',
+      company: 'Beetle Juice Company',
+    });
+
+    const changes = {
+      name: 'Jaques Bones',
+    };
+
+    const expected = {
+      reviewer_id: expect.any(String),
+      name: 'Jaques Bones',
+      company: 'Beetle Juice Company',
+    };
+
+    const res = await request(app)
+      .patch(`/api/v1/reviewers/${reviewer.reviewer_id}`)
+      .send(changes);
 
     expect(res.body).toEqual(expected);
   });
