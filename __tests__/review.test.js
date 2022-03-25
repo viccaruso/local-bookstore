ba5eb7d8b258cb90b0385ef259afde010bf8ea40
Victor Caruso <vic.caruso@gmail.com>
2022-03-24 17:08:15 -0700

    Set up failing test for insert route on review

--- __tests__/review.test.js
+++ __tests__/review.test.js
@@ -13,4 +13,23 @@ describe('local-bookstore routes', () => {
     pool.end();
   });
 
+  it('should create a review in the review table', async () => {
+    const res = await request(app).post('/api/v1/reviews').send({
+      rating: 5,
+      reviewer_id: 1,
+      review: 'This book was exquisite',
+      book_id: 1,
+    });
+    
+    const expected = {
+      review_id: expect.any(String),
+      reviewer_id: 1,
+      rating: 5,
+      review: 'This book was exquisite',
+      book_id: 1,
+    };
+
+    expect(res.body).toEqual(expected);
+  });
+
 });
